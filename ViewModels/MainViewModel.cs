da373d6ae3378d96fbbbb61b9562671a54e63bad
cwalker <cwalker@oncology.local>
2020-08-12 09:20:24 -0700

    Add project files.

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace Cindy_PlanChecker.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        private string _title = "ESAPIX Starter Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainViewModel()
+        {
+
+        }
+    }
+}
