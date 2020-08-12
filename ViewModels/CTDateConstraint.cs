e13f0455b4191fab2b81a57259ce8d86d0bd2cb7
Yujiao Qin <yujiao.qin@gmail.com>
2020-08-12 11:03:27 -0700

    final

---
+++ ViewModels/CTDateConstraint.cs
@@ -0,0 +1,74 @@
+﻿
+
+using ESAPIX.Constraints;
+
+using ESAPIX.Extensions;
+
+using System;
+
+using VMS.TPS.Common.Model.API;
+
+using F = ESAPIX.Facade.API;
+
+namespace Cindy_PlanChecker.ViewModels
+{
+    public class CTDateConstraint : IConstraint
+
+    {
+
+        public string Name => "CT Date Checker";
+
+        public string FullName => "CT Date < 60 Days";
+
+
+
+        public ConstraintResult CanConstrain(PlanningItem pi)
+
+        {
+
+            var pq = new PQAsserter(pi);
+
+            return pq.HasImage().CumulativeResult;
+
+        }
+
+
+
+        public ConstraintResult Constrain(PlanningItem pi)
+
+        {
+
+            var image = pi.GetImage();
+
+
+
+            var diffDays = (DateTime.Now - image.CreationDateTime).Value.TotalDays;
+
+            var msg = $"CT is {diffDays} days old";
+
+
+
+            if (diffDays <= 60)
+
+            {
+
+                return new ConstraintResult(this, ResultType.PASSED, msg);
+
+            }
+
+            else
+
+            {
+
+                return new ConstraintResult(this, ResultType.ACTION_LEVEL_3, msg);
+
+            }
+        }
+
+    }
+}
+
+
+
+
+   
